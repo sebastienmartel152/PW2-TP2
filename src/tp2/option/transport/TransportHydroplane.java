572f5627ce847bc64d41295e493538956eb64d7c
Dominic Jobin <dominic.jobin@gmail.com>
2016-11-03 16:02:23 -0400

    Classe TransportHydroplane
    et modification de TransportBoat pour recevoir le nombre de personnes
    dans le constructeur

--- src/tp2/option/transport/TransportHydroplane.java
+++ src/tp2/option/transport/TransportHydroplane.java
@@ -2,13 +2,13 @@ package tp2.option.transport;
 
 import tp2.reservation.Reservation;
 
-public class TransportBoat extends TransportOption {
-	public static final double DEFAULT_COST_PER_PERSON = 30.0;
+public class TransportHydroplane extends TransportOption {
+	public static final double DEFAULT_COST_PER_PERSON = 150.0;
 	private static double unitCostPerPerson = DEFAULT_COST_PER_PERSON;
 	
 	private int nbPeople;
 	
-	public TransportBoat(Reservation reservation, int nbPeople) {
+	public TransportHydroplane(Reservation reservation, int nbPeople) {
 		super(reservation);
 		
 		this.nbPeople = nbPeople;
@@ -16,7 +16,7 @@ public class TransportBoat extends TransportOption {
 
 	@Override
 	public double getUnitCost() {
-		return TransportBoat.unitCostPerPerson * this.nbPeople;
+		return TransportHydroplane.unitCostPerPerson * this.nbPeople;
 	}
 	
 
