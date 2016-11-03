12b4531e707ebcf5704990a8ccc940ed08c239d8
Dominic Jobin <dominic.jobin@gmail.com>
2016-11-03 16:42:53 -0400

    MealSupper

--- src/tp2/option/meal/MealSupper.java
+++ src/tp2/option/meal/MealSupper.java
@@ -2,24 +2,24 @@ package tp2.option.meal;
 
 import tp2.reservation.Reservation;
 
-public class MealBreakfastDinner extends MealOption {
-	public final static double DEFAULT_COST_PER_PERSON = 10;
+public class MealSupper extends MealOption {
+	public final static double DEFAULT_COST_PER_PERSON = 18;
 	private static double unitCostPerPerson = DEFAULT_COST_PER_PERSON;
 	
 	private int nbPeople;
 	
-	public MealBreakfastDinner(Reservation reservation, int nbPeople) {
+	public MealSupper(Reservation reservation, int nbPeople) {
 		super(reservation);
 		this.nbPeople = nbPeople;
 	}
 
 	@Override
 	public double getUnitCost() {
-		return MealBreakfastDinner.unitCostPerPerson * this.nbPeople;
+		return MealSupper.unitCostPerPerson * this.nbPeople;
 	}
 	
 	public static void setUnitCost(double cost){
-		MealBreakfastDinner.unitCostPerPerson = cost;
+		MealSupper.unitCostPerPerson = cost;
 	}
 
 }
