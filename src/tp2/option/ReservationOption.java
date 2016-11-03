76a84f92cf1858499fc965cd2c7789dd7bfa10b5
Dominic Jobin <dominic.jobin@gmail.com>
2016-11-03 15:55:08 -0400

    Classe ReservationOption

---
+++ src/tp2/option/ReservationOption.java
@@ -0,0 +1,15 @@
+package tp2.option;
+
+import tp2.reservation.Reservation;
+
+public abstract class ReservationOption implements Reservation{
+	private Reservation currentReservation;
+	
+	public ReservationOption(Reservation reservation){
+		this.currentReservation = reservation;
+	}
+	
+	public double calculateCost(){
+		return this.currentReservation.calculateTotalCost() + this.getUnitCost();
+	}
+}
