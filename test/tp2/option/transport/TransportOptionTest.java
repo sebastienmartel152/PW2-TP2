24d9be1a75236e3493a122da96ca01e6c1505e8b
Dominic Jobin <dominic.jobin@gmail.com>
2016-11-08 12:44:27 -0500

    TransportOptionMock et classe de test

---
+++ test/tp2/option/transport/TransportOptionTest.java
@@ -0,0 +1,28 @@
+package tp2.option.transport;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import tp2.cottage.CottageMock;
+import tp2.reservation.Reservation;
+
+public class TransportOptionTest {
+	private static final int ANY_NUMBER_OF_PEOPLE = 3;
+	
+	@Test
+	public void TransportDecorator_calculateTotalCost_shouldAddCostToBaseCottageCost() {
+		// Arrange
+		Reservation cottage = new CottageMock(ANY_NUMBER_OF_PEOPLE);
+		Reservation cottageWithTransport = new TransportOptionMock(cottage);
+		
+		final double EXPECTED_PRICE = cottage.calculateTotalCost() + TransportOptionMock.ANY_COST;
+		
+		// Act
+		double actualPrice = cottageWithTransport.calculateTotalCost();
+		
+		// Assert
+		assertEquals(EXPECTED_PRICE, actualPrice, 0.00);
+	}
+
+}
