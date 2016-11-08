0a06670441b69e20d8f2e90b6568e5754a209fe1
Dominic Jobin <dominic.jobin@gmail.com>
2016-11-08 12:28:33 -0500

    Refactoring MealOption
    Ajout de numberOfDays

--- src/tp2/option/meal/MealSupper.java
+++ src/tp2/option/meal/MealSupper.java
@@ -3,23 +3,20 @@ package tp2.option.meal;
 import tp2.reservation.Reservation;
 
 public class MealSupper extends MealOption {
-	public final static double DEFAULT_COST_PER_PERSON = 18;
-	private static double unitCostPerPerson = DEFAULT_COST_PER_PERSON;
+	public final static double DEFAULT_COST_PER_PERSON_PER_DAY = 18;
+	private static double unitCostPerPersonPerDay = DEFAULT_COST_PER_PERSON_PER_DAY;
 	
-	private int nbPeople;
-	
-	public MealSupper(Reservation reservation, int nbPeople) {
-		super(reservation);
-		this.nbPeople = nbPeople;
+	public MealSupper(Reservation reservation, int numberOfPeople, int numberOfDays) {
+		super(reservation, numberOfPeople, numberOfDays);
 	}
 
 	@Override
 	public double getUnitCost() {
-		return MealSupper.unitCostPerPerson * this.nbPeople;
+		return MealSupper.unitCostPerPersonPerDay * super.getNumberOfPeople() * super.getNumberOfDays();
 	}
 	
 	public static void setUnitCost(double cost){
-		MealSupper.unitCostPerPerson = cost;
+		MealSupper.unitCostPerPersonPerDay = cost;
 	}
 
 }
