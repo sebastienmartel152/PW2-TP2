7e6dcfea34499d3c516e7f9e6dd5502c905f5f33
Dominic Jobin <dominic.jobin@gmail.com>
2016-11-08 12:44:27 -0500

    TransportOptionMock et classe de test

---
+++ test/tp2/option/transport/TransportOptionMock.java
@@ -0,0 +1,17 @@
+package tp2.option.transport;
+
+import tp2.reservation.Reservation;
+
+public class TransportOptionMock extends TransportOption {
+	public static final double ANY_COST = 15;
+	
+	public TransportOptionMock(Reservation reservation) {
+		super(reservation);
+	}
+
+	@Override
+	public double getUnitCost() {
+		return ANY_COST;
+	}
+
+}
