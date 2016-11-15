da112cd2b165c0b36ed36d0bfb76461d28f04b24
sebastienmartel152 <watermarshal@hotmail.com>
2016-11-15 11:49:44 -0500

    Deals

---
+++ src/tp2/option/deal/FishingDeal.java
@@ -0,0 +1,26 @@
+package tp2.option.deal;
+
+import tp2.option.ReservationOption;
+import tp2.reservation.Reservation;
+
+public class FishingDeal extends ReservationOption {
+	
+	public static final double DEFAULT_PERCENTAGE_OFF = 0.25;
+	private static double percentageOff = DEFAULT_PERCENTAGE_OFF;
+	private Reservation reservation;
+	
+	public FishingDeal(Reservation reservation){
+		super(reservation);
+		
+	}
+
+	@Override
+	public double getUnitCost() {
+		return this.reservation.calculateTotalCost() * (1 - percentageOff); 
+	}
+	
+	public void setPercentageOff(double newPercentage){
+		FishingDeal.percentageOff = newPercentage;
+	}
+
+}
