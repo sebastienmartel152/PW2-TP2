7ec1aad6f18cf3dd0955d74ccac03dab7d6f6199
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-03 16:55:25 -0500

    Création de ReservationController et intégration dans WelcomeController

---
+++ src/tp3/controller/ReservationController.java
@@ -0,0 +1,18 @@
+package tp3.controller;
+
+import tp3.model.reservation.repository.ReservationRepository;
+
+public class ReservationController {
+	
+	private ReservationRepository repository;
+	
+	public ReservationController(ReservationRepository repository){
+		this.repository = repository;
+		
+	}
+	
+	public void displayWindow(){
+		
+	}
+	
+}
--- src/tp3/controller/WelcomeController.java
+++ src/tp3/controller/WelcomeController.java
@@ -6,9 +6,11 @@ import tp3.view.WelcomeView;
 public class WelcomeController {
 	
 	private ReservationRepository repository;
+	private ReservationController reservationController;
 	
 	public WelcomeController(ReservationRepository repository) {
 		this.repository = repository;
+		this.reservationController = new ReservationController(this.repository);
 	}
 	
 	public void startApplication(){
@@ -22,7 +24,7 @@ public class WelcomeController {
 	}
 
 	public void showReservationView() {
-		
+		this.reservationController.displayWindow();
 		
 	}
 
