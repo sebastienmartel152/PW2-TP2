e3a31722743419a9d64d86b676fbb8e8caca21c4
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-08 17:35:25 -0500

    DTOActivities et début de SendInformation()

---
+++ src/tp3/view/DTO/DTOActivities.java
@@ -0,0 +1,13 @@
+package tp3.view.DTO;
+
+public class DTOActivities {
+	public final boolean flyFishing;
+	public final boolean blackBearObservation;
+	public final boolean wolfObservation;
+	
+	public DTOActivities(boolean flyFishing, boolean blackBearObservation, boolean wolfObservation){
+		this.flyFishing = flyFishing;
+		this.blackBearObservation = blackBearObservation;
+		this.wolfObservation = wolfObservation;
+	}
+}
