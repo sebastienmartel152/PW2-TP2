e32fdce039bf43ac19a5431493118344795e2f35
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-11 02:45:53 -0500

    Création de la vue

---
+++ src/tp3/view/reservation/ReservationVerificationView.java
@@ -0,0 +1,25 @@
+package tp3.view.reservation;
+
+import tp3.controller.ReservationController;
+
+@SuppressWarnings("serial")
+public class ReservationVerificationView extends ReservationView {
+
+	private ReservationController reservationController;
+	
+	public ReservationVerificationView(ReservationController reservationController){
+		super();
+		this.reservationController = reservationController;
+		
+		
+		
+	}
+	
+	
+	@Override
+	public void sendInformation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
