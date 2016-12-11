82725f50dafa56254d997e98bcdea5c4257a28ba
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-11 09:22:04 -0500

    Ajout de la valeur numérique du mois

--- src/tp3/view/DTO/Months.java
+++ src/tp3/view/DTO/Months.java
@@ -1,16 +1,18 @@
 package tp3.view.DTO;
 
 public enum Months {
-	JANUARY("Janvier", 31), FEBRUARY("Février", 28), MARCH("Mars", 31), APRIL("Avril", 30),
-	MAY("Mai", 31), JUNE("Juin", 30), JULY("Juillet", 31), AUGUST("Août", 31),
-	SEPTEMBER("Septembre", 30), OCTOBER("Octobre", 31), NOVEMBER("Novembre", 30), DECEMBER("Décembre", 31);
+	JANUARY("Janvier", 31, 1), FEBRUARY("Février", 28, 2), MARCH("Mars", 31, 3), APRIL("Avril", 30, 4),
+	MAY("Mai", 31, 5), JUNE("Juin", 30, 6), JULY("Juillet", 31, 7), AUGUST("Août", 31, 8),
+	SEPTEMBER("Septembre", 30, 9), OCTOBER("Octobre", 31, 10), NOVEMBER("Novembre", 30, 11), DECEMBER("Décembre", 31, 12);
 	
 	private String monthName;
 	private int maxNbDays;
+	private int numericValue;
 	
-	private Months(String monthName, int maxNbDays){
+	private Months(String monthName, int maxNbDays, int numericValue){
 		this.monthName = monthName;
 		this.maxNbDays = maxNbDays;
+		this.numericValue = numericValue;
 	}
 	
 	public String toString(){
@@ -19,5 +21,9 @@ public enum Months {
 	
 	public int getMaxNbDays(){
 		return this.maxNbDays;
+	}
+	
+	public int getNumericValue(){
+		return this.numericValue;
 	}
 }
