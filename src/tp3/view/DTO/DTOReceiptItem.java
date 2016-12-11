cea157d49f2907eaf83064c2badc3503db9e2a17
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-11 14:02:02 -0500

    Correction du DTO

--- src/tp3/view/DTO/DTOReceiptItem.java
+++ src/tp3/view/DTO/DTOReceiptItem.java
@@ -1,8 +1,8 @@
 package tp3.view.DTO;
 
 public class DTOReceiptItem {
-	public static final String description;
-	public static final double price;
+	public final String description;
+	public final double price;
 	
 	public DTOReceiptItem(String description, double price){
 		this.description = description;
