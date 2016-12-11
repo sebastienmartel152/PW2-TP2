d05b9265ca65d6b685f92c99a682679e3cb21e87
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-11 09:25:57 -0500

    Enum Months renommé en Month

--- src/tp3/view/DTO/Month.java
+++ src/tp3/view/DTO/Month.java
@@ -1,6 +1,6 @@
 package tp3.view.DTO;
 
-public enum Months {
+public enum Month {
 	JANUARY("Janvier", 31, 1), FEBRUARY("Février", 28, 2), MARCH("Mars", 31, 3), APRIL("Avril", 30, 4),
 	MAY("Mai", 31, 5), JUNE("Juin", 30, 6), JULY("Juillet", 31, 7), AUGUST("Août", 31, 8),
 	SEPTEMBER("Septembre", 30, 9), OCTOBER("Octobre", 31, 10), NOVEMBER("Novembre", 30, 11), DECEMBER("Décembre", 31, 12);
@@ -9,7 +9,7 @@ public enum Months {
 	private int maxNbDays;
 	private int numericValue;
 	
-	private Months(String monthName, int maxNbDays, int numericValue){
+	private Month(String monthName, int maxNbDays, int numericValue){
 		this.monthName = monthName;
 		this.maxNbDays = maxNbDays;
 		this.numericValue = numericValue;
