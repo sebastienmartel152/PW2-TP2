a7016c6b739269a99fc7b05c475f1d904b6b78b0
Dominic Jobin <dominic.jobin@gmail.com>
2016-12-11 13:58:52 -0500

    DTOReceiptItem

---
+++ src/tp3/view/DTO/DTOReceiptItem.java
@@ -0,0 +1,11 @@
+package tp3.view.DTO;
+
+public class DTOReceiptItem {
+	public static final String description;
+	public static final double price;
+	
+	public DTOReceiptItem(String description, double price){
+		this.description = description;
+		this.price = price;
+	}
+}
